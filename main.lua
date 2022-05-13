355aa4e541370b0c57966804fd0182adc6bdd544
zcmace <zcmace@gmail.com>
2022-05-13 04:04:06 -0400

    working on tear delay bug

--- main.lua
+++ main.lua
@@ -2,12 +2,12 @@ local MyMod = RegisterMod("ZachsItems", 1);
 local myPlayer = Isaac.GetPlayer();
 local lore = Isaac.GetItemIdByName("Prophet's Lore");
 
-
+local debugText="";
 
 ------------- Prophets Lore ITEM
 function MyMod:LoreUpdate(player, cacheFlag)
     
-
+    
     local num = player:GetCollectibleNum(lore, true);
 
 
@@ -21,9 +21,13 @@ function MyMod:LoreUpdate(player, cacheFlag)
     end
 
     if cacheFlag == CacheFlag.CACHE_FIREDELAY  then 
+        
         if player:HasCollectible(lore) then
-            player.MaxFireDelay = player.MaxFireDelay - (1 * num);
+            if (player.MaxFireDelay - 1) >= 5 then
+                player.MaxFireDelay = player.MaxFireDelay - (1 * num);
+            end
         end
+        debugText = tostring(player.MaxFireDelay);
     end
 
 end
@@ -32,3 +36,9 @@ MyMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, MyMod.LoreUpdate);
 
 -------------
 
+-- debug
+local function onRender()
+    Isaac.RenderText(debugText, 150,130, 1, 1, 1, 255)
+end
+
+MyMod:AddCallback(ModCallbacks.MC_POST_RENDER, onRender)

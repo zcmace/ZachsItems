177565c90da604d79fc71b915f3f9d6dcda9f948
zcmace <zcmace@gmail.com>
2022-05-13 00:34:30 -0400

    basic skeleton

--- main.lua
+++ main.lua
@@ -1,1 +1,18 @@
-local MyMod
+local MyMod = RegisterMod("ZachsItems", 1)
+
+local book = Isaac.GetItemIdByName("Prophet's Lore")
+
+
+function MyMod:CacheUpdate(player, CacheFlag)
+    local player = Isaac.GetPlayer(0)
+    if player.HasCollectible(book) then
+        if CacheFlag == CacheFlag.CACHE_DAMAGE then
+            player.Damage = player.Damage +_1
+        end
+        if CacheFlag == CacheFlag.CACHE_FIREDELAY then
+            player.FireDelay = player.FireDelay +_1
+        end
+    end
+end
+
+MyMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, MyMod.CacheUpdate)
